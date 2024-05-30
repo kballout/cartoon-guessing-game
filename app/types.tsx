cb8bf90f9d7210b64d9eae19f6e78013ee3b06bb
Kassim Ballout <54900156+kballout@users.noreply.github.com>
2024-05-30 12:26:04 -0400

    added character and option types

--- app/types.tsx
+++ app/types.tsx
@@ -1,3 +1,14 @@
-type Character = {
-    
+export type Character = {
+    name: string,
+    path: string
+}
+
+export enum VALIDATION {
+    "INCORRECT",
+    "CORRECT",
+}
+
+export type Option = {
+    name: string,
+    validation: VALIDATION
 }
